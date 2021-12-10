1b605bb1369ff2757ff5dca2ee57fdfa49d9e43c
imsaif77 <saifidrisi88@gmail.com>
2021-12-10 23:12:51 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/gall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,182 @@
+import 'dart:io';
+import 'dart:io' as io;
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await firebase_core.Firebase.initializeApp();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'NA Upload',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: UploadingImageToFirebaseStorage(),
+  ));
+}
+
+final Color green = Colors.green;
+final Color orange = Colors.orange;
+
+class UploadingImageToFirebaseStorage extends StatefulWidget {
+  @override
+  _UploadingImageToFirebaseStorageState createState() =>
+      _UploadingImageToFirebaseStorageState();
+}
+
+class _UploadingImageToFirebaseStorageState
+    extends State<UploadingImageToFirebaseStorage> {
+  File? _imageFile = null;
+  List<XFile>? _imageFileList;
+
+  ///NOTE: Only supported on Android & iOS
+  ///Needs image_picker plugin {https://pub.dev/packages/image_picker}
+  final picker = ImagePicker();
+
+  Future pickImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = File(pickedFile!.path);
+    });
+  }
+
+  Future uploadImageToFirebase(BuildContext context) async {
+    String fileName = basename(_imageFile!.path);
+    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+        .ref()
+        .child('uploads')
+        .child('/$fileName');
+
+    final metadata = firebase_storage.SettableMetadata(
+        contentType: 'image/*', customMetadata: {'picked-file-path': fileName});
+
+    firebase_storage.UploadTask uploadTask;
+
+    //late StorageUploadTask uploadTask = firebaseStorageRef.putFile(_imageFile);
+    uploadTask = ref.putFile(io.File(_imageFile!.path), metadata);
+
+    firebase_storage.UploadTask task = await Future.value(uploadTask);
+    Future.value(uploadTask)
+        .then((value) => {print("Upload file path ${value.ref.fullPath}")})
+        .onError((error, stackTrace) =>
+            {print("Upload file path error ${error.toString()} ")});
+  }
+
+  String? _retrieveDataError;
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      setState(() {
+        // _imageFile = ! response.file;
+        // _imageFileList = response.files;
+      });
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 360,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(250.0),
+                    bottomRight: Radius.circular(10.0)),
+                gradient: LinearGradient(
+                    colors: [green, orange],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "Uploading Image to Firebase Storage",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: double.infinity,
+                        margin: const EdgeInsets.only(
+                            left: 30.0, right: 30.0, top: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: _imageFile != null
+                              ? Image.file(_imageFile!)
+                              : FlatButton(
+                                  child: Icon(
+                                    Icons.add_a_photo,
+                                    color: Colors.blue,
+                                    size: 50,
+                                  ),
+                                  onPressed: pickImage,
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                uploadImageButton(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget uploadImageButton(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            margin: const EdgeInsets.only(
+                top: 30, left: 20.0, right: 20.0, bottom: 20.0),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [orange, green],
+                ),
+                borderRadius: BorderRadius.circular(30.0)),
+            child: FlatButton(
+              onPressed: () => uploadImageToFirebase(context),
+              child: Text(
+                "Upload Image",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+/*
+if request.auth != null*/
